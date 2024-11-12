31d9c3426540cbfe3babe7da186215d9abeeeb54
Prateekpritish <pritishpappa@gmail.com>
2024-11-12 19:53:21 +0530

    resume.c

---
+++ 12 nov 2024/resume.c
@@ -0,0 +1,48 @@
+\documentclass[a4paper,22pt]{article}
+\usepackage{geometry}
+\usepackage{enumitem}
+\geometry{left=1in, right=1in, top=1in, bottom=1in}
+
+\setlength{\parindent}{0pt}  
+
+\begin{document}
+
+\begin{center}
+    {\LARGE \textbf{PRITISH SARKAR}} \\
+    Doomdooma, Tinsukia, Assam \\
+    Phone: 8011897680 \\
+    Email: pritish@gmail.com \\
+\end{center}
+
+
+\section*{Objective}
+I am fresher and ready to land a job.
+
+
+\section*{Experience}
+\textbf{Teacher} \hfill HUL \\
+\textit{Tinsukia, Assam} \hfill 23 sep 2023 \\
+\begin{itemize}[noitemsep]
+    \item Tabla.
+    \item Computer.
+\end{itemize}
+
+
+\section*{Education}
+\textbf{Degree, BSC IT} \hfill JIST \\
+Sotai, Assam \hfill april 2023
+
+
+\section*{Skills}
+\begin{itemize}[noitemsep]
+    \item Programming
+    \item Tabla
+    \item Editing
+\end{itemize}
+
+
+\section*{Projects}
+\textbf{Development} \\
+web development applications.
+
+\end{document}
