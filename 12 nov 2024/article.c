47c964e5c51af2dbf0a3a8d91e17db6c59a37c9b
Prateekpritish <pritishpappa@gmail.com>
2024-11-12 19:52:01 +0530

    article.c

---
+++ 12 nov 2024/article.c
@@ -0,0 +1,40 @@
+\documentclass{article} 
+\usepackage[utf8]{inputenc} 
+\usepackage{amsmath} 
+\usepackage{graphicx} 
+
+
+\title{C programming} 
+\author{Pritish Sarkar} 
+\date{\today} 
+
+\begin{document}
+
+\maketitle 
+
+\begin{abstract}
+C is a very good language we should learn.
+
+\end{abstract}
+
+\section{Introduction}
+This is the introduction section. Provide background information and state the purpose of your article.
+
+\section{Main Content}
+This section is where the main content goes. Use subsections for organization.
+
+\subsection{Subsection 1}
+Details for the first subsection.
+
+\subsection{Subsection 2}
+Details for the second subsection.
+
+\section{Conclusion}
+Summarize the key points and conclude your article here.
+
+\begin{thebibliography}{9}
+\bibitem{sample_reference}
+Pritish Sarkar, \textit{C Programming}, Pritish sarkar, 2024.
+\end{thebibliography}
+
+\end{document}
